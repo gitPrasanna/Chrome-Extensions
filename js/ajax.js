5bb752c76d11da323f561e619abf7aeaa39ee950
gitPradeep <pradeepprasanna.rajapaksha4@gmail.com>
2015-06-08 17:10:50 +0530

    type your commit message here

---
+++ js/ajax.js
@@ -0,0 +1,116 @@
+
+function function_login (username, password) {
+
+	$.ajax({
+		url: 'http://localhost/tpro/index.php/api/miniaccessapi/auth',
+		type: 'POST',
+		cache: false,
+	    async: true,
+	    crossdomain: true,
+		dataType: 'json',
+		data: {username: username, password: password},
+		success:function(response){
+			if (response.query_status=='success') { // check user login process is success and if yes, set all user data into view.
+
+				// $.session.set('user_id', 	$.trim(response.user.member_id));
+				// $.session.set('user_name', 	$.trim(response.user.username));
+				// $.session.set('user_fname', $.trim(response.user.first_name));
+				// $.session.set('user_lname', $.trim(response.user.last_name));
+				// $.session.set('division', 	$.trim(response.division.division));
+				// $.session.set('division_code', 	$.trim(response.division.division_code));
+				// $.session.set('designation',$.trim(response.user.designation.designation));
+
+				$.cookie('user_id', 		$.trim(response.user.member_id), 	{ expires: 1 });
+				$.cookie('user_name', 		$.trim(response.user.username), 	{ expires: 1 });
+				$.cookie('user_fname', 		$.trim(response.user.first_name), 	{ expires: 1 });
+				$.cookie('user_lname', 		$.trim(response.user.last_name), 	{ expires: 1 });
+				$.cookie('division', 		$.trim(response.division.division), { expires: 1 });
+				$.cookie('division_code', 	$.trim(response.division.division_code), { expires: 1 });
+				$.cookie('designation', 	$.trim(response.user.designation.designation), { expires: 1 });
+
+				$('.dash-date-time p#userdata small').text($.cookie('user_fname')+ ' ' + $.cookie('user_lname') +' ['+ $.cookie('designation') +']');	// set designation
+
+				$('.d-sui-tile h1 span').text(response.sui.sui);	// set sui value
+				$('.c-sui-tile h1 span').text(response.sui.c_sui);	// set cumulative sui value
+
+				$('.working-project h5 span').text('The '+ $.cookie('division') +' ['+ $.cookie('division_code') +'] division');	// division name & set division code
+				
+				//for (var i = 0; i < response.projects.length; i++) {			// set project list
+				for (var i = 0; i < 5; i++) {			// set project list
+					if (response.projects[i].project_name!='') {
+						$('.working-project-list ul').append('<li>'+ $.trim(response.projects[i].project_name) +'</li>');
+					};
+				};
+
+				$('div.priority-1 p.task').text($.trim('[High Prioruty]') + ' ' + $.trim('Insert OSBT web site content and do SEO').substring(0,30)+'...');	// set cumulative sui value
+				$('div.priority-2 p.task').text($.trim('[Moderate Prioruty]') + ' ' + $.trim('OSBT web site content update (IDSD Progression Path)').substring(0,30)+'...');	// set cumulative sui value
+
+				$('#login-msgs').removeClass('alert-danger').removeClass('alert-success').removeClass('alert-warning').addClass('alert-success').html('<strong>Yeah!</strong> We are good.').delay(3000);
+
+				$('#dash-board').css('display', 'block');
+				$('#login-page').css('display', 'none');
+
+			}else{
+
+			};
+	    },
+	    error:function(jqXHR,textStatus,errorThrown){
+	    	console.log(jqXHR);
+	    }
+	});
+
+}
+
+function get_user_data (username) {
+
+	$.ajax({
+		url: 'http://localhost/tpro/index.php/api/miniaccessapi/getuserdata',
+		type: 'POST',
+		cache: false,
+	    async: true,
+	    crossdomain: true,
+		dataType: 'json',
+		data: {username: username},
+		success:function(response){
+			if (response.query_status=='success') { // check user login process is success and if yes, set all user data into view.
+
+				$.cookie('user_id', 		$.trim(response.user.member_id), 	{ expires: 1 });
+				$.cookie('user_name', 		$.trim(response.user.username), 	{ expires: 1 });
+				$.cookie('user_fname', 		$.trim(response.user.first_name), 	{ expires: 1 });
+				$.cookie('user_lname', 		$.trim(response.user.last_name), 	{ expires: 1 });
+				$.cookie('division', 		$.trim(response.division.division), { expires: 1 });
+				$.cookie('division_code', 	$.trim(response.division.division_code), { expires: 1 });
+				$.cookie('designation', 	$.trim(response.user.designation.designation), { expires: 1 });
+
+				$('.dash-date-time p#userdata small').text($.cookie('user_fname')+ ' ' + $.cookie('user_lname') +' ['+ $.cookie('designation') +']');	// set designation
+
+				$('.d-sui-tile h1 span').text(response.sui.sui);	// set sui value
+				$('.c-sui-tile h1 span').text(response.sui.c_sui);	// set cumulative sui value
+
+				$('.working-project h5 span').text('The '+ $.cookie('division') +' ['+ $.cookie('division_code') +'] division');	// division name & set division code
+				
+				//for (var i = 0; i < response.projects.length; i++) {			// set project list
+				for (var i = 0; i < 5; i++) {			// set project list
+					if (response.projects[i].project_name!='') {
+						$('.working-project-list ul').append('<li>'+ $.trim(response.projects[i].project_name) +'</li>');
+					};
+				};
+
+				$('div.priority-1 p.task').text($.trim('[High Prioruty]') + ' ' + $.trim('Insert OSBT web site content and do SEO').substring(0,30)+'...');	// set cumulative sui value
+				$('div.priority-2 p.task').text($.trim('[Moderate Prioruty]') + ' ' + $.trim('OSBT web site content update (IDSD Progression Path)').substring(0,30)+'...');	// set cumulative sui value
+
+				$('#login-msgs').removeClass('alert-danger').removeClass('alert-success').removeClass('alert-warning').addClass('alert-success').html('<strong>Yeah!</strong> We are good.').delay(3000);
+
+				$('#dash-board').css('display', 'block');
+				$('#login-page').css('display', 'none');
+
+			}else{
+
+			};
+	    },
+	    error:function(jqXHR,textStatus,errorThrown){
+	    	console.log(jqXHR);
+	    }
+	});
+
+}
--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
 
+	if(typeof($.cookie('user_name')) != "undefined" && $.cookie('user_name') !== null) {
+    	
+		// alert($.cookie('user_id'));
+		get_user_data ($.cookie('user_name'))
+
+	}else{
+		$('#login-msgs').removeClass('alert-danger').addClass('alert-success').html('<strong>Hello!</strong> Welcome to mini access.')
+	
+	}
+
 	$(document).on('submit', '#login-form', function(event) {
 		event.preventDefault();
-		// $(this).serializeArray();
-		load_dash_board();
+
+		if($('#username').val()=='' || $('#password').val()==''){  // check login form is filled or not 
+			// if not send error msg
+			$('#login-msgs').removeClass('alert-success').addClass('alert-danger').html('<strong>Oh snap!</strong> You have to enter login data.')
+		}
+		else{ // if yes send processing msg and call to login function
+			$('#login-msgs').removeClass('alert-success').removeClass('alert-danger').addClass('alert-warning').html('<strong>Please wait!</strong> Um processing.')
+			function_login($('#username').val(), $('#password').val());
+		}
 
 	});
 
 	$(".live-tile, .flip-list").not(".exclude").liveTile();
 	
 	//play on hover with initial play
-	var $tiles = $("#tile3").liveTile({ 
+	var $tiles = $("#tile1, #tile3").liveTile({ 
 	    playOnHover:true,
 	    repeatCount: 0,
 	    delay: 0,
@@ -25,35 +42,45 @@ $(document).ready(function() {
 	    $(ele).liveTile("play", delay);
 	});
 
+	$(document).on('click', '#logout', function(event) {
+		event.preventDefault();
+
+		$.removeCookie('user_id');
+		$.removeCookie('user_name');
+		$.removeCookie('user_fname');
+		$.removeCookie('user_lname');
+		$.removeCookie('division');
+		$.removeCookie('division_code');
+		$.removeCookie('designation');
+
+		$('#login-msgs').removeClass('alert-danger').removeClass('alert-success').removeClass('alert-warning').addClass('alert-success').html('<strong>Yeah!</strong> You have successfully logout.').delay(3000);
+		$('#login-page').css('display', 'block');
+		$('#dash-board').css('display', 'none');
+	});
+
 
 });
 
-	var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-	function GetClock(){
-		var d=new Date();
-		var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
-		if(nyear<1000) nyear+=1900;
+var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+function GetClock(){
+	var d=new Date();
+	var nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+	if(nyear<1000) nyear+=1900;
 
-		var d=new Date();
-		var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+	var d=new Date();
+	var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
 
-		if(nhour==0){ap=" AM";nhour=12;}
-		else if(nhour<12){ap=" AM";}
-		else if(nhour==12){ap=" PM";}
-		else if(nhour>12){ap=" PM";nhour-=12;}
+	if(nhour==0){ap=" AM";nhour=12;}
+	else if(nhour<12){ap=" AM";}
+	else if(nhour==12){ap=" PM";}
+	else if(nhour>12){ap=" PM";nhour-=12;}
 
-		if(nmin<=9) nmin="0"+nmin;
-		if(nsec<=9) nsec="0"+nsec;
+	if(nmin<=9) nmin="0"+nmin;
+	if(nsec<=9) nsec="0"+nsec;
 
-		document.getElementById('date_time').innerHTML=""+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
-		}
-		window.onload=function(){
-		GetClock();
-		setInterval(GetClock,1000);
+	document.getElementById('date_time').innerHTML=""+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
 	}
-/*	
-	$(document).ready(
-		function() { 
-			$("html").niceScroll();
-		}
-	);*/
+	window.onload=function(){
+	GetClock();
+	setInterval(GetClock,1000);
+}
